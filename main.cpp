f756929c283ed66539d8758284d7f18a59e3b556
Aidan Dodds <aidan@codeplay.com>
2019-08-18 21:38:11 +0100

    replace doubles with floats

--- main.cpp
+++ main.cpp
@@ -74,6 +74,8 @@ SDL_Surface *surf;
 
 //draw the pixels of the stripe as a vertical line
 void verLine(int x, int drawStart, int drawEnd, uint32_t color) {
+  drawStart = SDL_max(drawStart, 0);
+  drawEnd = SDL_min(drawEnd, h - 1);
   uint32_t *p = (uint32_t*)surf->pixels;
   p += x;
   p += drawStart * surf->w;
@@ -98,20 +100,20 @@ uint32_t wallColour(int wall) {
   }
 }
 
-void raycast(int x, double cameraX, double posX, double posY, double rayDirX,
-             double rayDirY) {
+void raycast(int x, float cameraX, float posX, float posY, float rayDirX,
+             float rayDirY) {
   // which box of the map we're in
   int mapX = int(posX);
   int mapY = int(posY);
 
   // length of ray from current position to next x or y-side
-  double sideDistX;
-  double sideDistY;
+  float sideDistX;
+  float sideDistY;
 
   // length of ray from one x or y-side to next x or y-side
-  double deltaDistX = std::abs(1.f / rayDirX);
-  double deltaDistY = std::abs(1.f / rayDirY);
-  double perpWallDist;
+  float deltaDistX = std::abs(1.f / rayDirX);
+  float deltaDistY = std::abs(1.f / rayDirY);
+  float perpWallDist;
 
   // what direction to step in x or y-direction (either +1 or -1)
   int stepX;
@@ -125,14 +127,14 @@ void raycast(int x, double cameraX, double posX, double posY, double rayDirX,
     sideDistX = (posX - mapX) * deltaDistX;
   } else {
     stepX = 1;
-    sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+    sideDistX = (mapX + 1.0f - posX) * deltaDistX;
   }
   if (rayDirY < 0) {
     stepY = -1;
     sideDistY = (posY - mapY) * deltaDistY;
   } else {
     stepY = 1;
-    sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+    sideDistY = (mapY + 1.0f - posY) * deltaDistY;
   }
   // perform DDA
   while (hit == 0) {
@@ -162,18 +164,14 @@ void raycast(int x, double cameraX, double posX, double posY, double rayDirX,
 
   // calculate lowest and highest pixel to fill in current stripe
   int drawStart = -lineHeight / 2 + h / 2;
-  if (drawStart < 0)
-    drawStart = 0;
-  int drawEnd = lineHeight / 2 + h / 2;
-  if (drawEnd >= h)
-    drawEnd = h - 1;
+  int drawEnd   =  lineHeight / 2 + h / 2;
 
   // choose wall color
   int color = wallColour(worldMap[mapX][mapY]);
 
   // give x and y sides different brightness
   if (side == 1) {
-    color = color / 2;
+    color = (color >> 1) & 0x7f7f7f7f;
   }
 
   // draw the pixels of the stripe as a vertical line
@@ -182,12 +180,12 @@ void raycast(int x, double cameraX, double posX, double posY, double rayDirX,
 
 int main(int argc, char *args[])
 {
-  double posX = 22, posY = 12;  //x and y start position
-  double dirX = -1, dirY = 0; //initial direction vector
-  double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+  float posX = 22, posY = 12;  //x and y start position
+  float dirX = -1, dirY = 0; //initial direction vector
+  float planeX = 0, planeY = 0.66f; //the 2d raycaster version of camera plane
 
-  double time = 0; //time of current frame
-  double oldTime = 0; //time of previous frame
+  uint32_t time = 0; //time of current frame
+  uint32_t oldTime = 0; //time of previous frame
 
   SDL_Init(SDL_INIT_VIDEO);
   surf = SDL_SetVideoMode(w, h, 32, 0);
@@ -207,9 +205,9 @@ int main(int argc, char *args[])
     for(int x = 0; x < w; x++)
     {
       // calculate ray position and direction
-      double cameraX = 2 * x / double(w) - 1; // x-coordinate in camera space
-      double rayDirX = dirX + planeX * cameraX;
-      double rayDirY = dirY + planeY * cameraX;
+      float cameraX = 2 * x / float(w) - 1; // x-coordinate in camera space
+      float rayDirX = dirX + planeX * cameraX;
+      float rayDirY = dirY + planeY * cameraX;
 
       raycast(x, cameraX, posX, posY, rayDirX, rayDirY);
     }
@@ -217,14 +215,14 @@ int main(int argc, char *args[])
     //timing for input and FPS counter
     oldTime = time;
     time = SDL_GetTicks();
-    double frameTime = (time - oldTime) / 1000.0; //frameTime is the time this frame has taken, in seconds
+    float frameTime = (time - oldTime) / 1000.f; //frameTime is the time this frame has taken, in seconds
 
     SDL_Flip(surf);
     SDL_FillRect(surf, nullptr, 0x101010);
 
     //speed modifiers
-    double moveSpeed = frameTime * 5.0; //the constant value is in squares/second
-    double rotSpeed = frameTime * 3.0; //the constant value is in radians/second
+    float moveSpeed = frameTime * 5.0f; //the constant value is in squares/second
+    float rotSpeed = frameTime * 3.0f; //the constant value is in radians/second
 
     const uint8_t *keys = SDL_GetKeyState(nullptr);
 
@@ -244,10 +242,10 @@ int main(int argc, char *args[])
     if (keys[SDLK_RIGHT])
     {
       //both camera direction and camera plane must be rotated
-      double oldDirX = dirX;
+      float oldDirX = dirX;
       dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
       dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
-      double oldPlaneX = planeX;
+      float oldPlaneX = planeX;
       planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
       planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
     }
@@ -255,10 +253,10 @@ int main(int argc, char *args[])
     if (keys[SDLK_LEFT])
     {
       //both camera direction and camera plane must be rotated
-      double oldDirX = dirX;
+      float oldDirX = dirX;
       dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
       dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
-      double oldPlaneX = planeX;
+      float oldPlaneX = planeX;
       planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
       planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
     }
