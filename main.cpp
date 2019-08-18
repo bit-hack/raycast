5b967d883a187e262b795735144ba36187898090
Aidan Dodds <aidan@codeplay.com>
2019-08-18 21:32:44 +0100

    move things into functions

--- main.cpp
+++ main.cpp
@@ -67,6 +67,9 @@ int worldMap[mapWidth][mapHeight]=
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
+const int w = 512;
+const int h = 384;
+
 SDL_Surface *surf;
 
 //draw the pixels of the stripe as a vertical line
@@ -80,6 +83,103 @@ void verLine(int x, int drawStart, int drawEnd, uint32_t color) {
   }
 }
 
+uint32_t wallColour(int wall) {
+  switch (wall) {
+  case 1:
+    return 0xff0000;
+  case 2:
+    return 0x00ff00;
+  case 3:
+    return 0x0000ff;
+  case 4:
+    return 0xffffff;
+  default:
+    return 0x00ffff;
+  }
+}
+
+void raycast(int x, double cameraX, double posX, double posY, double rayDirX,
+             double rayDirY) {
+  // which box of the map we're in
+  int mapX = int(posX);
+  int mapY = int(posY);
+
+  // length of ray from current position to next x or y-side
+  double sideDistX;
+  double sideDistY;
+
+  // length of ray from one x or y-side to next x or y-side
+  double deltaDistX = std::abs(1.f / rayDirX);
+  double deltaDistY = std::abs(1.f / rayDirY);
+  double perpWallDist;
+
+  // what direction to step in x or y-direction (either +1 or -1)
+  int stepX;
+  int stepY;
+
+  int hit = 0; // was there a wall hit?
+  int side;    // was a NS or a EW wall hit?
+  // calculate step and initial sideDist
+  if (rayDirX < 0) {
+    stepX = -1;
+    sideDistX = (posX - mapX) * deltaDistX;
+  } else {
+    stepX = 1;
+    sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+  }
+  if (rayDirY < 0) {
+    stepY = -1;
+    sideDistY = (posY - mapY) * deltaDistY;
+  } else {
+    stepY = 1;
+    sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+  }
+  // perform DDA
+  while (hit == 0) {
+    // jump to next map square, OR in x-direction, OR in y-direction
+    if (sideDistX < sideDistY) {
+      sideDistX += deltaDistX;
+      mapX += stepX;
+      side = 0;
+    } else {
+      sideDistY += deltaDistY;
+      mapY += stepY;
+      side = 1;
+    }
+    // Check if ray has hit a wall
+    if (worldMap[mapX][mapY] > 0)
+      hit = 1;
+  }
+  // Calculate distance projected on camera direction (Euclidean distance will
+  // give fisheye effect!)
+  if (side == 0)
+    perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
+  else
+    perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
+
+  // Calculate height of line to draw on screen
+  int lineHeight = (int)(h / perpWallDist);
+
+  // calculate lowest and highest pixel to fill in current stripe
+  int drawStart = -lineHeight / 2 + h / 2;
+  if (drawStart < 0)
+    drawStart = 0;
+  int drawEnd = lineHeight / 2 + h / 2;
+  if (drawEnd >= h)
+    drawEnd = h - 1;
+
+  // choose wall color
+  int color = wallColour(worldMap[mapX][mapY]);
+
+  // give x and y sides different brightness
+  if (side == 1) {
+    color = color / 2;
+  }
+
+  // draw the pixels of the stripe as a vertical line
+  verLine(x, drawStart, drawEnd, color);
+}
+
 int main(int argc, char *args[])
 {
   double posX = 22, posY = 12;  //x and y start position
@@ -89,11 +189,8 @@ int main(int argc, char *args[])
   double time = 0; //time of current frame
   double oldTime = 0; //time of previous frame
 
-  const int w = 512;
-  const int h = 384;
-
   SDL_Init(SDL_INIT_VIDEO);
-  surf = SDL_SetVideoMode(512, 384, 32, 0);
+  surf = SDL_SetVideoMode(w, h, 32, 0);
 
   bool done = false;
 
@@ -109,98 +206,12 @@ int main(int argc, char *args[])
 
     for(int x = 0; x < w; x++)
     {
-      //calculate ray position and direction
-      double cameraX = 2 * x / double(w) - 1; //x-coordinate in camera space
+      // calculate ray position and direction
+      double cameraX = 2 * x / double(w) - 1; // x-coordinate in camera space
       double rayDirX = dirX + planeX * cameraX;
       double rayDirY = dirY + planeY * cameraX;
-      //which box of the map we're in
-      int mapX = int(posX);
-      int mapY = int(posY);
 
-      //length of ray from current position to next x or y-side
-      double sideDistX;
-      double sideDistY;
-
-       //length of ray from one x or y-side to next x or y-side
-      double deltaDistX = std::abs(1.f / rayDirX);
-      double deltaDistY = std::abs(1.f / rayDirY);
-      double perpWallDist;
-
-      //what direction to step in x or y-direction (either +1 or -1)
-      int stepX;
-      int stepY;
-
-      int hit = 0; //was there a wall hit?
-      int side; //was a NS or a EW wall hit?
-      //calculate step and initial sideDist
-      if (rayDirX < 0)
-      {
-        stepX = -1;
-        sideDistX = (posX - mapX) * deltaDistX;
-      }
-      else
-      {
-        stepX = 1;
-        sideDistX = (mapX + 1.0 - posX) * deltaDistX;
-      }
-      if (rayDirY < 0)
-      {
-        stepY = -1;
-        sideDistY = (posY - mapY) * deltaDistY;
-      }
-      else
-      {
-        stepY = 1;
-        sideDistY = (mapY + 1.0 - posY) * deltaDistY;
-      }
-      //perform DDA
-      while (hit == 0)
-      {
-        //jump to next map square, OR in x-direction, OR in y-direction
-        if (sideDistX < sideDistY)
-        {
-          sideDistX += deltaDistX;
-          mapX += stepX;
-          side = 0;
-        }
-        else
-        {
-          sideDistY += deltaDistY;
-          mapY += stepY;
-          side = 1;
-        }
-        //Check if ray has hit a wall
-        if (worldMap[mapX][mapY] > 0) hit = 1;
-      }
-      //Calculate distance projected on camera direction (Euclidean distance will give fisheye effect!)
-      if (side == 0) perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
-      else           perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
-
-      //Calculate height of line to draw on screen
-      int lineHeight = (int)(h / perpWallDist);
-
-      //calculate lowest and highest pixel to fill in current stripe
-      int drawStart = -lineHeight / 2 + h / 2;
-      if(drawStart < 0)drawStart = 0;
-      int drawEnd = lineHeight / 2 + h / 2;
-      if(drawEnd >= h)drawEnd = h - 1;
-
-      //choose wall color
-      int color;
-      switch(worldMap[mapX][mapY])
-      {
-        case 1:  color = 0xff0000; break; //red
-        case 2:  color = 0x00ff00; break; //green
-        case 3:  color = 0x0000ff; break; //blue
-        case 4:  color = 0xffffff; break; //white
-        default: color = 0x00ffff; break; //yellow
-      }
-
-      //give x and y sides different brightness
-      if (side == 1) {color = color / 2;}
-
-      //draw the pixels of the stripe as a vertical line
-      verLine(x, drawStart, drawEnd, color);
+      raycast(x, cameraX, posX, posY, rayDirX, rayDirY);
     }
 
     //timing for input and FPS counter
