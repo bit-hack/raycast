072621dc4d1a9956e6788cc9e5706155849ab5b9
Aidan Dodds <aidan@codeplay.com>
2019-08-22 20:29:47 +0100

    add variable height walls and floor

--- main.cpp
+++ main.cpp
@@ -1,26 +1,3 @@
-/*
-Copyright (c) 2004-2007, Lode Vandevenne
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 #define _SDL_main_h
 #include <SDL/SDL.h>
 
@@ -29,51 +6,51 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <vector>
 #include <iostream>
 
-/*
-g++ *.cpp -lSDL -O3 -W -Wall -ansi -pedantic
-g++ *.cpp -lSDL
-*/
-
-//place the example code below here:
-
-#define mapWidth 24
+#define mapWidth  24
 #define mapHeight 24
 
-int worldMap[mapWidth][mapHeight]=
-{
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+uint8_t worldMap[mapWidth][mapHeight] = {
+  {9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9},
+  {9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,9},
+  {9,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,1,0,3,0,0,0,9},
+  {9,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,9},
+  {9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,0,0,0,0,0,1,2,3,4,5,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,0,0,0,0,0,1,2,3,4,5,0,0,0,1,1,1,0,0,0,0,0,0,9},
+  {9,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,1,0,0,0,0,0,0,9},
+  {9,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,9},
+  {9,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,4,0,0,0,0,7,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9},
+  {9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9}
 };
 
-const int w = 512;
-const int h = 384;
+const int w = 320;
+const int h = 240;
+
+float playerX = 22.f;
+float playerY = 12.f;
+
+float playerDX = -1;
+float playerDY =  0;
+
+float eyeLevel = 3.f;
+float nearPlaneDist = .66f;
 
 SDL_Surface *surf;
 
-//draw the pixels of the stripe as a vertical line
-void verLine(int x, int drawStart, int drawEnd, uint32_t color) {
+static void verLine(int x, int drawStart, int drawEnd, uint32_t color) {
   drawStart = SDL_max(drawStart, 0);
   drawEnd = SDL_min(drawEnd, h - 1);
   uint32_t *p = (uint32_t*)surf->pixels;
@@ -85,180 +62,215 @@ void verLine(int x, int drawStart, int drawEnd, uint32_t color) {
   }
 }
 
-uint32_t wallColour(int wall) {
-  switch (wall) {
-  case 1:
-    return 0xff0000;
-  case 2:
-    return 0x00ff00;
-  case 3:
-    return 0x0000ff;
-  case 4:
-    return 0xffffff;
-  default:
-    return 0x00ffff;
+static float fpart(float x) {
+  return x - float(int32_t(x));
+}
+
+static float ipart(float x) {
+  return float(int32_t(x));
+}
+
+static float project(float y, float dist) {
+  if (dist < 0.01f) {
+    return float(h);
+  }
+  else {
+    return (h / 2.f) - 64 * (y - eyeLevel) / dist;
   }
 }
 
-void raycast(int x, float cameraX, float posX, float posY, float rayDirX,
-             float rayDirY) {
-  // which box of the map we're in
-  int mapX = int(posX);
-  int mapY = int(posY);
+static void draw_vert(int32_t x, float y1, float y2, uint32_t rgb) {
+  verLine(x, int32_t(y1), int32_t(y2), rgb);
+}
 
-  // length of ray from current position to next x or y-side
-  float sideDistX;
-  float sideDistY;
+void raycast(
+  uint32_t x,
+  float px, float py,
+  float rx, float ry) {
 
-  // length of ray from one x or y-side to next x or y-side
-  float deltaDistX = std::abs(1.f / rayDirX);
-  float deltaDistY = std::abs(1.f / rayDirY);
-  float perpWallDist;
+  enum axis_t { axis_x, axis_y };
 
-  // what direction to step in x or y-direction (either +1 or -1)
-  int stepX;
-  int stepY;
+  // which grid cell we are in
+  int intx = int32_t(px);
+  int inty = int32_t(py);
 
-  int hit = 0; // was there a wall hit?
-  int side;    // was a NS or a EW wall hit?
-  // calculate step and initial sideDist
-  if (rayDirX < 0) {
-    stepX = -1;
-    sideDistX = (posX - mapX) * deltaDistX;
-  } else {
-    stepX = 1;
-    sideDistX = (mapX + 1.0f - posX) * deltaDistX;
-  }
-  if (rayDirY < 0) {
-    stepY = -1;
-    sideDistY = (posY - mapY) * deltaDistY;
-  } else {
-    stepY = 1;
-    sideDistY = (mapY + 1.0f - posY) * deltaDistY;
-  }
-  // perform DDA
-  while (hit == 0) {
-    // jump to next map square, OR in x-direction, OR in y-direction
-    if (sideDistX < sideDistY) {
-      sideDistX += deltaDistX;
-      mapX += stepX;
-      side = 0;
+  // length between axis strides
+  const float xdelta = fabsf(1.f / rx);  // x stride
+  const float ydelta = fabsf(1.f / ry);  // y stride
+
+  // axis step
+  const int32_t stepX = (rx >= 0) ? 1 : -1;
+  const int32_t stepY = (ry >= 0) ? 1 : -1;
+
+  // length accumulator
+  float xlen = xdelta * ((rx < 0) ? fpart(px) : 1.f - fpart(px));
+  float ylen = ydelta * ((ry < 0) ? fpart(py) : 1.f - fpart(py));
+
+  // axis travel per grid cell
+  const float ddx = rx / fabsf(ry);
+  const float ddy = ry / fabsf(rx);
+
+  // starting point for x axis intersections
+  float pxx = (rx > 0) ? ipart(px) : ipart(1.f + px);
+  const float pdx = pxx - px;
+  float pxy = py + pdx * (ry / rx);
+
+  // starting point for y axis intersections
+  float pyy = (ry > 0) ? ipart(py) : ipart(1.f + py);
+  const float pdy = pyy - py;
+  float pyx = px + pdy * (rx / ry);
+
+  // perpendicular ray distance
+  float pdist = 0.f;
+
+  float oldy = h;
+  float oldHeight = 0.f;
+
+  axis_t axis = axis_x;
+  while (true) {
+
+    if (xlen < ylen) {
+      axis = axis_x;
+      // step ray
+      xlen += xdelta;
+      intx += stepX;
+      // step intersection point
+      pxy += ddy;
+      pxx += stepX;
+      // calculate perp distance
+      const float distx = (intx - px) + (stepX < 0 ? 1 : 0);
+      pdist = distx / rx;
+
     } else {
-      sideDistY += deltaDistY;
-      mapY += stepY;
-      side = 1;
+      axis = axis_y;
+      // step ray
+      ylen += ydelta;
+      inty += stepY;
+      // step intersection point
+      pyx += ddx;
+      pyy += stepY;
+      // calculate perp distance
+      const float disty = (inty - py) + (stepY < 0 ? 1 : 0);
+      pdist = disty / ry;
     }
-    // Check if ray has hit a wall
-    if (worldMap[mapX][mapY] > 0)
-      hit = 1;
+
+    const uint8_t height = worldMap[intx][inty];
+
+    float nexty = oldy;
+
+    // project all edges as we need them for correct texture interpolation
+
+    // draw floor tile
+    {
+      const float newy = project(SDL_max(oldHeight, height), pdist);
+      draw_vert(x, newy, oldy, ((intx ^ inty) & 1) ? 0x202020 : 0x303030);
+      nexty = SDL_min(oldy, newy);
+    }
+
+    // draw wall
+    if (height > oldHeight) {
+      const float y0 = project(oldHeight, pdist);
+      const float y1 = project(height, pdist);
+      const uint32_t color = (axis == axis_x) ? 0xffffff : 0x7f7f7f;
+      draw_vert(x, y1, SDL_min(oldy, y0), color);
+      nexty = SDL_min(oldy, y1);
+    }
+
+    oldy = nexty;
+    oldHeight = height;
+
+    // check map tile for collision
+    if (height >= 9) {
+      break;
+    }
   }
-  // Calculate distance projected on camera direction (Euclidean distance will
-  // give fisheye effect!)
-  if (side == 0)
-    perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
-  else
-    perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
+}
 
-  // Calculate height of line to draw on screen
-  int lineHeight = (int)(h / perpWallDist);
+static void doMove(float moveSpeed, float rotSpeed) {
+  const uint8_t *keys = SDL_GetKeyState(nullptr);
 
-  // calculate lowest and highest pixel to fill in current stripe
-  int drawStart = -lineHeight / 2 + h / 2;
-  int drawEnd   =  lineHeight / 2 + h / 2;
-
-  // choose wall color
-  int color = wallColour(worldMap[mapX][mapY]);
-
-  // give x and y sides different brightness
-  if (side == 1) {
-    color = (color >> 1) & 0x7f7f7f7f;
+  // move forward if no wall in front of you
+  if (keys[SDLK_UP]) {
+    if (worldMap[int(playerX + playerDX * moveSpeed)][int(playerY)] == 0)
+      playerX += playerDX * moveSpeed;
+    if (worldMap[int(playerX)][int(playerY + playerDY * moveSpeed)] == 0)
+      playerY += playerDY * moveSpeed;
   }
-
-  // draw the pixels of the stripe as a vertical line
-  verLine(x, drawStart, drawEnd, color);
+  // move backwards if no wall behind you
+  if (keys[SDLK_DOWN]) {
+    if (worldMap[int(playerX - playerDX * moveSpeed)][int(playerY)] == 0)
+      playerX -= playerDX * moveSpeed;
+    if (worldMap[int(playerX)][int(playerY - playerDY * moveSpeed)] == 0)
+      playerY -= playerDY * moveSpeed;
+  }
+  // rotate to the right
+  if (keys[SDLK_RIGHT]) {
+    // both camera direction and camera plane must be rotated
+    const float oldDirX = playerDX;
+    const float oldDirY = playerDY;
+    playerDX = oldDirX * cosf(-rotSpeed) - oldDirY * sinf(-rotSpeed);
+    playerDY = oldDirX * sinf(-rotSpeed) + oldDirY * cosf(-rotSpeed);
+  }
+  // rotate to the left
+  if (keys[SDLK_LEFT]) {
+    // both camera direction and camera plane must be rotated
+    const float oldDirX = playerDX;
+    const float oldDirY = playerDY;
+    playerDX = oldDirX * cosf(rotSpeed) - oldDirY * sinf(rotSpeed);
+    playerDY = oldDirX * sinf(rotSpeed) + oldDirY * cosf(rotSpeed);
+  }
 }
 
 int main(int argc, char *args[])
 {
-  float posX = 22, posY = 12;  //x and y start position
-  float dirX = -1, dirY = 0; //initial direction vector
-  float planeX = 0, planeY = 0.66f; //the 2d raycaster version of camera plane
-
-  uint32_t time = 0; //time of current frame
-  uint32_t oldTime = 0; //time of previous frame
+  uint32_t oldTime = 0;
 
   SDL_Init(SDL_INIT_VIDEO);
   surf = SDL_SetVideoMode(w, h, 32, 0);
 
-  bool done = false;
-
-  while(!done)
+  for(bool done = false; !done;)
   {
+    // pump SDL events
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
         done = true;
         break;
       }
+      if (event.type == SDL_KEYDOWN) {
+        if (event.key.keysym.sym == SDLK_ESCAPE) {
+          done = true;
+          break;
+        }
+      }
     }
 
+    // for screen width
     for(int x = 0; x < w; x++)
     {
-      // calculate ray position and direction
-      float cameraX = 2 * x / float(w) - 1; // x-coordinate in camera space
-      float rayDirX = dirX + planeX * cameraX;
-      float rayDirY = dirY + planeY * cameraX;
-
-      raycast(x, cameraX, posX, posY, rayDirX, rayDirY);
+      // calculate ray direction
+      const float planeX =  playerDY * nearPlaneDist;
+      const float planeY = -playerDX * nearPlaneDist;
+      const float cameraX = 2.f * x / float(w) - 1.f;
+      const float rayDirX = playerDX + planeX * cameraX;
+      const float rayDirY = playerDY + planeY * cameraX;
+      raycast(x, playerX, playerY, rayDirX, rayDirY);
     }
 
-    //timing for input and FPS counter
-    oldTime = time;
-    time = SDL_GetTicks();
-    float frameTime = (time - oldTime) / 1000.f; //frameTime is the time this frame has taken, in seconds
-
+    // present the screen
     SDL_Flip(surf);
     SDL_FillRect(surf, nullptr, 0x101010);
+    SDL_Delay(5);
 
-    //speed modifiers
-    float moveSpeed = frameTime * 5.0f; //the constant value is in squares/second
-    float rotSpeed = frameTime * 3.0f; //the constant value is in radians/second
+    // timing for input and FPS counter
+    const uint32_t time = SDL_GetTicks();
+    const float frameTime = (time - oldTime) / 1000.f;
+    oldTime = time;
 
-    const uint8_t *keys = SDL_GetKeyState(nullptr);
+    // speed modifiers
+    const float moveSpeed = frameTime * 5.0f;
+    const float rotSpeed = frameTime * 3.0f;
 
-    //move forward if no wall in front of you
-    if (keys[SDLK_UP])
-    {
-      if(worldMap[int(posX + dirX * moveSpeed)][int(posY)] == false) posX += dirX * moveSpeed;
-      if(worldMap[int(posX)][int(posY + dirY * moveSpeed)] == false) posY += dirY * moveSpeed;
-    }
-    //move backwards if no wall behind you
-    if (keys[SDLK_DOWN])
-    {
-      if(worldMap[int(posX - dirX * moveSpeed)][int(posY)] == false) posX -= dirX * moveSpeed;
-      if(worldMap[int(posX)][int(posY - dirY * moveSpeed)] == false) posY -= dirY * moveSpeed;
-    }
-    //rotate to the right
-    if (keys[SDLK_RIGHT])
-    {
-      //both camera direction and camera plane must be rotated
-      float oldDirX = dirX;
-      dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
-      dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
-      float oldPlaneX = planeX;
-      planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
-      planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
-    }
-    //rotate to the left
-    if (keys[SDLK_LEFT])
-    {
-      //both camera direction and camera plane must be rotated
-      float oldDirX = dirX;
-      dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
-      dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
-      float oldPlaneX = planeX;
-      planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
-      planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
-    }
+    doMove(moveSpeed, rotSpeed);
   }
 }
